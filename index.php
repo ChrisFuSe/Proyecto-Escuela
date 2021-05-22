e431156e43b1ee2a0c2e860b170a86268f8e4d86
Christian <cfs.christianfuentes@gmail.com>
2021-05-22 14:35:53 -0500

    Agregadas imagenes y creado el fondo con estilos css

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Prestige Management System</title>
+        <link rel="shortcut icon" type="image/x-icon" href="assets/img/Prestige.ico"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="assets/css/estilos.css">
+    </head>
+    <body>
+    </body>
+</html>
