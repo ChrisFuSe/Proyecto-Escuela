001a42c2fa8a0d128f0fdb05fdd7c2be4f652530
Christian <cfs.christianfuentes@gmail.com>
2021-05-24 15:16:53 -0500

    Agregada la pestaña de home para la directora (puede que falte agregar las opciones que tiene el maestro)

--- index.php
+++ index.php
@@ -17,19 +17,20 @@
         </div>
 
         <div class="container" id="log_in">
-        <p id="titulo">BIENVENIDO AL SISTEMA DE GESTION DE PRESTIGE</p>
-        <form>
-            <div class="mb-3">
-                <label for="usuario" class="form-label">Usuario</label>
-                <input type="textbox" class="form-control" id="usuario">
-            </div>
-            <div class="mb-3">
-                <label for="contraseña" class="form-label">Contraseña</label>
-                <input type="password" class="form-control" id="contraseña" aria-describedby="ayuda_contraseña">
-            </div>
-            <div id="ayuda_contraseña" class="form-text"><a href="https://www.google.com.mx/" >¿Olvidaste tu contraseña?</a></div>
-            <button type="submit" class="btn btn-primary" id="ingresar">Ingresar</button>
-        </form>
+            <p id="titulo">BIENVENIDO AL SISTEMA DE GESTION DE PRESTIGE</p>
+            <form>
+                <div class="mb-3">
+                    <label for="usuario" class="form-label">Usuario</label>
+                    <input type="textbox" class="form-control" id="usuario">
+                </div>
+                <div class="mb-3">
+                    <label for="contraseña" class="form-label">Contraseña</label>
+                    <input type="password" class="form-control" id="contraseña" aria-describedby="ayuda_contraseña">
+                </div>
+                <div id="ayuda_contraseña" class="form-text"><a href="https://www.google.com.mx/" >¿Olvidaste tu contraseña?</a></div>
+                <button type="submit" class="btn btn-primary" id="ingresar">Ingresar</button>
+            </form>
         </div>
+        <div hidden>Iconos diseñados por <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.es/" title="Flaticon">www.flaticon.es</a></div>
     </body>
 </html>
