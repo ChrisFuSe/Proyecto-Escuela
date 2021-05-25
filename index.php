99a85fcbe08663110169b4b7482db0f8e976876b
Christian <cfs.christianfuentes@gmail.com>
2021-05-24 22:02:34 -0500

    Agregados comentarios para mejor entendimiento del código

--- index.php
+++ index.php
@@ -2,20 +2,30 @@
 <html>
     <head>
         <title>Prestige Management System</title>
+        <!--    Icono de pestaña    -->
         <link rel="shortcut icon" type="image/x-icon" href="assets/img/Prestige.ico"/>
+
+        <!--    Importacion de fuentes (Encontradas en GoogleFonts)     -->
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Petit+Formal+Script&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=PT+Serif&display=swap" rel="stylesheet">
+
+        <!--    Importacion del css de boostrap     -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+        <!--    Importacion del css (estilos propios)   -->
         <link rel="stylesheet" type="text/css" href="assets/css/estilos_constantes.css">
         <link rel="stylesheet" type="text/css" href="assets/css/estilos_log_in.css">
     </head>
     <body>
+
+    <!--    Cabecera con el logo y el nombre de nuestro sistema     -->
         <div class="container-fluid" id="cabecera">
             <img src="assets/img/Logo_Prestige.png" class="img-fluid" alt="Logo" width="140">
             <p id="nombre_sistema">Prestige Management System</p>
         </div>
 
+    <!--    Formulario para ingresar con usuario y contraseña     -->
         <div class="container" id="log_in">
             <p id="titulo">BIENVENIDO AL SISTEMA DE GESTION DE PRESTIGE</p>
             <form action="assets/html/home-directora.html">
@@ -31,6 +41,7 @@
                 <button type="submit" class="btn btn-primary" id="ingresar">Ingresar</button>
             </form>
         </div>
-        <div hidden>Iconos diseñados por <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.es/" title="Flaticon">www.flaticon.es</a></div>
+
+        <!--Reconocimiento a los autores de los iconos en oculto--><div hidden>Iconos diseñados por <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.es/" title="Flaticon">www.flaticon.es</a></div>
     </body>
 </html>
