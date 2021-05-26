9503e53294f04b23f8a73352535d097a2e2c5080
Christian <cfs.christianfuentes@gmail.com>
2021-05-26 14:21:03 -0500

    Actualizadas todas las clases a "botones-navegacion" dentro de las imagenes de cada pantalla y cambiada la animacion dentro del javascript

--- assets/js/animaciones-boton.js
+++ assets/js/animaciones-boton.js
@@ -2,30 +2,12 @@ let botones_navegacion = document.getElementsByClassName("botones-navegacion");
 if(botones_navegacion.length != 0){
     for (let indice in botones_navegacion){
         botones_navegacion[indice].addEventListener('mouseover' , function(){
-            botones_navegacion[indice].style.width = "7%";
+            botones_navegacion[indice].style.transition = "0.2s linear";
+            botones_navegacion[indice].style.transform = "scale(1.1)";
         });
         botones_navegacion[indice].addEventListener('mouseout' , function(){
-            botones_navegacion[indice].style.width = "5%";
-        });
-    }
-}
-
-let botones_opcion = document.getElementsByClassName("boton-opc");
-if(botones_opcion.length != 0){
-    for (let indice in botones_opcion){
-        botones_opcion[indice].addEventListener('mouseover' , function(){
-            if(botones_opcion[indice].ariaLabel == "Gestor Grupos"){
-                botones_opcion[indice].style.width = "82%";
-            }else{
-                botones_opcion[indice].style.width = "57%";
-            }
-        });
-        botones_opcion[indice].addEventListener('mouseout' , function(){
-            if(botones_opcion[indice].ariaLabel == "Gestor Grupos"){
-                botones_opcion[indice].style.width = "75%";
-            }else{
-                botones_opcion[indice].style.width = "50%";
-            }
+            botones_navegacion[indice].style.transition = "0.2s linear";
+            botones_navegacion[indice].style.transform = "scale(1)";
         });
     }
 }
