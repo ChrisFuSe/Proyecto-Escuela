d53b6bb94d8dec08d97c94aecba54c4998ca57e1
Christian <cfs.christianfuentes@gmail.com>
2021-05-26 17:56:01 -0500

    Actualizado el archivo de validaciones para que no se tomen en cuenta los inputs de tipo busqueda

--- assets/js/validacion.js
+++ assets/js/validacion.js
@@ -4,26 +4,28 @@ function validar(){
     let expReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
 
     for (let indice in cajas_texto){
-        if(cajas_texto[indice].value == ""){
-            validacion = false;
-            cajas_texto[indice].style.borderColor = "red";
-        }else{
-            cajas_texto[indice].style.borderColor = "gray";
-        }
-        if(cajas_texto[indice].ariaLabel == "Numero"){
-            if(isNaN (parseInt(cajas_texto[indice].value))){
+        if(cajas_texto[indice].ariaLabel != "Busqueda"){
+            if(cajas_texto[indice].value == ""){
                 validacion = false;
                 cajas_texto[indice].style.borderColor = "red";
             }else{
                 cajas_texto[indice].style.borderColor = "gray";
             }
-        }
-        if(cajas_texto[indice].ariaLabel == "Correo electronico"){
-            if(expReg.test(cajas_texto[indice].value) == false){
-                validacion = false;
-                cajas_texto[indice].style.borderColor = "red";
-            }else{
-                cajas_texto[indice].style.borderColor = "gray";
+            if(cajas_texto[indice].ariaLabel == "Numero"){
+                if(isNaN (parseInt(cajas_texto[indice].value))){
+                    validacion = false;
+                    cajas_texto[indice].style.borderColor = "red";
+                }else{
+                    cajas_texto[indice].style.borderColor = "gray";
+                }
+            }
+            if(cajas_texto[indice].ariaLabel == "Correo electronico"){
+                if(expReg.test(cajas_texto[indice].value) == false){
+                    validacion = false;
+                    cajas_texto[indice].style.borderColor = "red";
+                }else{
+                    cajas_texto[indice].style.borderColor = "gray";
+                }
             }
         }
     }
