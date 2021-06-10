ce94b363a27a31109e77a5bafd1a76373feaa548
PAULINA ITZEL CERVANTES MEDINA <l18121477@morelia.tecnm.mx>
2021-06-10 02:03:08 -0500

    consultar profesor

--- app/Http/Controllers/Consultar.php
+++ app/Http/Controllers/Consultar.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Profesor;
 
 class Consultar extends Controller
 {
-    //
+    public function consultarProfesor(Request $request){
+        $profesor = Profesor::find($request->id_profesor);
+        return view('gestores/maestros/consultar-eliminar-maestro', compact('profesor'));
+    }
 }
