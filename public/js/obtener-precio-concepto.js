003b3ec31961f4d7c7263afc5af02630acc24da6
Christian <cfs.christianfuentes@gmail.com>
2021-06-11 09:47:01 -0500

    Se le envían los conceptos almacenados en la base de datos al formulario y agregado el ocultar en base al tipo de pago

---
+++ public/js/obtener-precio-concepto.js
@@ -0,0 +1,25 @@
+concepto.addEventListener('change',() =>{
+    $.ajax({
+        headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
+        type: 'GET',
+        dataType: 'json',
+        url: '',
+        data: {
+            '_token': $('input[name=_token]').val(),
+            'concepto': $('#concepto').val(),
+        },
+        beforeSend: function () {
+            console.log('bloqueo botones');
+        },
+        complete: function () {
+            console.log('desbloqueo botones');
+        },
+        success: function (response) {
+           console.log('ok!');
+           console.log(response);
+        },
+        error: function (jqXHR) {
+            console.log('boo!');
+        }
+    });
+});
