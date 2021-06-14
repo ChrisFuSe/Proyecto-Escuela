6d6f47a334fbcdce148b087bfcb6481cd6e8ef93
Christian <cfs.christianfuentes@gmail.com>
2021-06-14 09:02:29 -0500

    Actualizado el menu lateral

--- resources/views/templates/pagina-menu-botones.blade.php
+++ resources/views/templates/pagina-menu-botones.blade.php
@@ -12,6 +12,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Petit+Formal+Script&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=PT+Serif&display=swap" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
 
     <!--    Favicon     -->
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset('img/Prestige.ico') }}" />
@@ -20,12 +21,25 @@
     @yield('estilos')
     <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.css') }}">
     <link rel="stylesheet" href="{{ asset('css/estilos_constantes.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/menu.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/estilos-menu.css') }}">
     <link rel="stylesheet" href="{{ asset('css/estilos_imagen_opcion.css') }}">
     <link rel="stylesheet" href="{{ asset('css/estilos_home.css') }}">
-    
+
     <!--    Header scripts      -->
     @yield('header-scripts')
+
+    <!--    Script para el menu lateral     -->
+    <script>
+        function openNav() {
+            document.getElementById("sideNavigation").style.width = "250px";
+            document.getElementById("main").style.marginLeft = "250px";
+        }
+         
+        function closeNav() {
+            document.getElementById("sideNavigation").style.width = "0";
+            document.getElementById("main").style.marginLeft = "0";
+        }
+    </script>
 </head>
 
 <body>
@@ -42,63 +56,73 @@
 
 
     <!--    Menu para navegación rápida     -->
-    <input type="checkbox" id="btm-menu">
-    <label for="btm-menu" id="titulo" class="h1"><img id="icomenu" src="{{ asset('img/icono-menu.png') }}"
-            alt="Menu">@yield('operacion')</label>
-    <nav class="menu" style="z-index:1;">
-        <ul>
-            <?php
+
+
+    <div id="sideNavigation" class="sidenav">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <?php
                 switch(auth()->user()->tipo){
                     case '1':
                     ?>
-            <li><a href="\gestores\maestros">Maestros</a></li>
-            <li><a href="\gestores\pagos">Pagos</a></li>
-            <li><a href="\gestores\grupos">Grupos</a></li>
-            <li><a href="\gestores\alumnos">Alumnos</a></li>
-            <li><a href="\gestores\usuarios">Usuarios</a></li>
-            <li><a href="\gestores\cal-as">Calificaciones-asistencia</a></li>
-            <?php
+        <a href="\gestores\maestros">Maestros</a>
+        <a href="\gestores\pagos">Pagos</a>
+        <a href="\gestores\grupos">Grupos</a>
+        <a href="\gestores\alumnos">Alumnos</a>
+        <a href="\gestores\usuarios">Usuarios</a>
+        <a href="\gestores\cal-as">Calificaciones-asistencia</a>
+        <?php
                         break;
                     case '2':
                     ?>
-            <li><a href="\gestores\maestros">Maestros</a></li>
-            <li><a href="\gestores\pagos">Pagos</a></li>
-            <li><a href="\gestores\alumnos">Alumnos</a></li>
-            <li><a href="\gestores\cal-as">Calificaciones-asistencia</a></li>
-            <?php
+        <a href="\gestores\maestros">Maestros</a>
+        <a href="\gestores\pagos">Pagos</a>
+        <a href="\gestores\alumnos">Alumnos</a>
+        <a href="\gestores\cal-as">Calificaciones-asistencia</a>
+        <?php
                         break;
                     case '3':
                     ?>
-            <li><a href="\gestores\cal-as">Calificaciones-asistencia</a></li>
-            <?php
+        <a href="\gestores\cal-as">Calificaciones-asistencia</a>
+        <?php
                         break;
                 }
                 ?>
-            <li><a style="margin-left: 10%" href="{{route('login.salir')}}">Cerrar Sesion</a></li>
-        </ul>
-    </nav>
-
-    @yield('cuerpo')
-
-    <!--    Botones Home y Regresar      -->
-    <div class="col" style="margin-top: 3%;">
-        <div class="btn-real" style="display: inline; padding-right: 5%;">
-            <img width="5%" src="{{ asset('img/icono-regresar.png') }}" class="botones-navegacion"
-                onclick="window.history.back();">
-            <img width="5%" src="{{ asset('img/icono-home.png') }}" class="botones-navegacion"
-                onclick="window.location.href = '{{route('home')}}'">
-        </div>
+        <a style="margin-left: 10%" href="{{route('login.salir')}}">Cerrar Sesion</a>
     </div>
 
-    <!--    Scripts      -->
-    <script src="{{ asset('js/animaciones-boton.js') }}"></script>
-    @yield('scripts')
+    <nav class="topnav">
+        <a href="#" onclick="openNav()">
+            <svg width="30" height="30" id="icoOpen">
+                <path d="M0,5 30,5" stroke="#000" stroke-width="5" />
+                <path d="M0,14 30,14" stroke="#000" stroke-width="5" />
+                <path d="M0,23 30,23" stroke="#000" stroke-width="5" />
+            </svg>
+        </a>
+    </nav>
 
-    <!--    Reconocimiento a los autores de los iconos en oculto    -->
-    <div hidden>Iconos diseñados por <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a
-            href="https://www.flaticon.es/" title="Flaticon">www.flaticon.es</a></div>
-    <div hidden>Icons made by <a href="https://www.flaticon.com/authors/roundicons" title="Roundicons">Roundicons</a>
-        from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+    <div id="main">
+        @yield('cuerpo')
+
+        <!--    Botones Home y Regresar      -->
+        <div class="col" style="margin-top: 3%;">
+            <div class="btn-real" style="display: inline; padding-right: 5%;">
+                <img width="5%" src="{{ asset('img/icono-regresar.png') }}" class="botones-navegacion"
+                    onclick="window.history.back();">
+                <img width="5%" src="{{ asset('img/icono-home.png') }}" class="botones-navegacion"
+                    onclick="window.location.href = '{{route('home')}}'">
+            </div>
+        </div>
+
+        <!--    Scripts      -->
+        <script src="{{ asset('js/animaciones-boton.js') }}"></script>
+        @yield('scripts')
+
+        <!--    Reconocimiento a los autores de los iconos en oculto    -->
+        <div hidden>Iconos diseñados por <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a
+                href="https://www.flaticon.es/" title="Flaticon">www.flaticon.es</a></div>
+        <div hidden>Icons made by <a href="https://www.flaticon.com/authors/roundicons" title="Roundicons">Roundicons</a>
+            from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+    </div>
 </body>
 
 </html>
